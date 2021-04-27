ad0b839eeb2d1e46785d7e2888ed332a20c25a88
DESKTOP-2VFB9CV\tke20 <tke2000wow@gmail.com>
2021-04-27 12:54:54 +0200

    ShowsCarNow

--- app.js
+++ app.js
@@ -1,17 +1,35 @@
-const express = require("express");
-const logger = require("morgan");
+const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(
-logger("dev", {
-    // hvis ALLE requests skal ses i loggen, kan næste linje udkommenteres
-})
-);
-app.get("/", (req, res) => {
-res.send("Hello World!");
+const logger = require('morgan');
+app.use(logger('dev',{
+	skip: req => !req.url.endsWith('.html') && req.url.indexOf('.') > -1
+}));
+
+app.set('view engine', 'ejs');
+app.set('views','./views');
+
+app.use(express.static('./public'));
+
+app.get('/', (req, res)=>{
+    let car = {
+        MakeModel: 'Bmw E60 M5',
+        year: 2010,
+        nicecar: true,
+    };
+	res.render('index', {
+        message: 'Hello Wrld!',
+        data: [1,2,3,4],
+        Model: "E92 M3",
+        Make: "Bmw",
+        nice: true,
+        car: car,
+    });
 });
 
-app.listen(port, () => {
-console.log(`Server running http://localhost:${port}`);
+
+
+app.listen(port,()=>{
+	console.log(`Server running... http://localhost:${port}`);
 });
